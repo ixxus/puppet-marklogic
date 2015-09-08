356c2eb982305dd9ff6a49732e52fd488edff817
Jon Evans <jon.evans@ixxus.com>
2015-09-08 15:58:04 +0100

    Fix syntax. || -> or

--- manifests/marklogic.pp
+++ manifests/marklogic.pp
@@ -71,7 +71,7 @@ class marklogic::marklogic (
         refreshonly => true,
         subscribe   => Package['MarkLogic'],
       }
-    } elsif ($version =~ /^7/ || $version =~ /^8/) {
+    } elsif ($version =~ /^7/ or $version =~ /^8/) {
       # MarkLogic V7 on EC2 requires Java, but not to run. Unknown what for, but
       # it's used highly in /opt/MarkLogic/mlcmd/bin/mlcmd. It seems to only be
       # needed if using EC2 optimization. To disable EC2 detection and get running
